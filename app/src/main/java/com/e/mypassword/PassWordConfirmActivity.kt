dfbe8a38a64f34fca8009e69edbb683b12805289
imacocokoga-ayabe <ancientsuzaku@gmail.com>
2021-09-22 13:03:13 +0900

    [2021/09/22]@2.0h 入力と表示

--- app/src/main/java/com/e/mypassword/PassWordConfirmActivity.kt
+++ app/src/main/java/com/e/mypassword/PassWordConfirmActivity.kt
@@ -3,12 +3,42 @@ package com.e.mypassword
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_pass_word_confirm.*
 
 class PassWordConfirmActivity : AppCompatActivity() {
+
+    private var siteName: String = ""
+    private var webSite: String = ""
+    private var mailAddress: String = ""
+    private var account: String = ""
+    private var passWord: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pass_word_confirm)
+
+        siteName = intent.getStringExtra("siteName_key").toString()
+        webSite = intent.getStringExtra("webSite_key").toString()
+        mailAddress = intent.getStringExtra("mailAddress_key").toString()
+        account = intent.getStringExtra("account_key").toString()
+        passWord = intent.getStringExtra("passWord_key").toString()
+
+        val siteNameTextView: TextView = findViewById(R.id.passWord_confirm_siteName_textView_id)
+        siteNameTextView.text = siteName
+
+        val webSiteTextView: TextView = findViewById(R.id.passWord_confirm_webSite_textView_id)
+        webSiteTextView.text = webSite
+
+        val mailAddressTextView: TextView = findViewById(R.id.passWord_confirm_mailAddress_textView_id)
+        mailAddressTextView.text = mailAddress
+
+        val accountTextView: TextView = findViewById(R.id.passWord_confirm_account_textView_id)
+        accountTextView.text = account
+
+        val passWordTextView: TextView = findViewById(R.id.passWord_confirm_passWord_textView_id)
+        passWordTextView.text = passWord
+
 
         password_confirm_return_top_screen_button_id.setOnClickListener {
             val intent = Intent(this, TopScreenActivity::class.java)
--- app/src/main/java/com/e/mypassword/PassWordInputActivity.kt
+++ app/src/main/java/com/e/mypassword/PassWordInputActivity.kt
@@ -3,16 +3,37 @@ package com.e.mypassword
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_pass_word_input.*
 
 class PassWordInputActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pass_word_input)
 
         password_input_confirm_button_id.setOnClickListener {
+
+            val siteName = findViewById<EditText>(R.id.passWord_input_siteName_editTextPersonName_id).text
+            val webSite = findViewById<EditText>(R.id.passWord_input_webSite_editTextTextPersonName_id).text
+            val mailAddress = findViewById<EditText>(R.id.passWord_input_mailAddress_editTextTextEmailAddress_id).text
+            val account = findViewById<EditText>(R.id.passWord_input_account_editTextTextMultiLine_id).text
+            val passWord = findViewById<EditText>(R.id.passWord_input_passWord_editTextTextMultiLine_id).text
+
             val intent = Intent(this, PassWordConfirmActivity::class.java)
+
+            intent.putExtra("siteName_key", siteName.toString())
+            intent.putExtra("webSite_key", webSite.toString())
+            intent.putExtra("mailAddress_key", mailAddress.toString())
+            intent.putExtra("account_key", account.toString())
+            intent.putExtra("passWord_key", passWord.toString())
+
             startActivity(intent)
         }
+
+
+
+
+
     }
 }
